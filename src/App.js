2ccab246a43183c9a810779009c88d6f790cfd05
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-11-19 00:35:27 +0300

    modif-4

--- src/App.js
+++ src/App.js
@@ -3,32 +3,41 @@ import Aos from 'aos'
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
 import Home from './Pages/Home';
-import Portfolio from './Components/Portfolio';
+import Sidebar from './Components/Sidebar';
 import Footer from './Components/footer';
+import { BrowserRouter as Router , Link , Route ,Routes} from 'react-router-dom';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
 function App() {
   useEffect(()=>{
     Aos.init({duration:2000})
   },[])
   return (
     <div className="App">
-     
+      
+     <Router>
+     <Sidebar />
       <section>
           <div className="navbar-container">
-
               <div className="navbar" data-aos="fade-up">
                 <div className="logo-div">
-                <i class="fas fa-stream menu-toggler"></i>
-                  <i class="fas fa-tablets logo"></i>
-                  <h1 className="brand"><strong className="abdul">abdul</strong> <strong className="aziz">aziz</strong></h1>
-                  <i class="fas fa-tablets logo1"></i>
+                  <div> <i class="fas fa-stream menu-toggler"></i></div>
+                  <div className="lgDiv">
+                     
+                      <i class="fas fa-tablets logo"></i>
+                      <Link to="/" className="link">
+                      <h1 className="brand"><strong className="abdul">abdul</strong> <strong className="aziz">aziz</strong></h1>
+                      </Link>
+                  </div>
                 </div>
                 
                 <div className="nav-menu">
                   <ul>
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>About</li>
-                    <li><a href="#portfolio" ><button className="btn btn-success">Portfolio</button></a></li>
+                    <li><Link to="/" className="link">Home</Link></li>
+                    <li><Link to="/about" className="link">About</Link></li>
+                    <li><Link to="/contact" className="link">Contact</Link></li>
+                   
+                    <li><a href="#portfolio" className="link"><button className="btn btn-success">Portfolio</button></a></li>
                   </ul>
                 </div>
               </div>
@@ -36,20 +45,16 @@ function App() {
       </section>
 
       <section>
-          <Home />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/contact" exact element={<Contact />} />
+        <Route path="/about" exact element={<About />} />
+      </Routes>
       </section>
-
-      <section>
-      <div className="portfolio" id="portfolio">
-        <div className="container">
-         <Portfolio />
-        </div>
-      </div>
-      </section>
-
       <section>
           <Footer />
       </section>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+    return (
+
+         <div className="side-container">
+            <div className="sidebar">
+                <h1>hello</h1>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti optio asperiores exercitationem eum fugit veritatis nulla obcaecati eius excepturi quas dolore, aperiam porro cum officia cupiditate ipsam perspiciatis accusamus est.
+            </div>
+         </div> 
+         
+         );
+}
+
+export default Sidebar;
