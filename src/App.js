48a71e6ee58147b761f35d948d994df9f40f29c0
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-11-18 08:49:47 +0300

    modif-2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Aos from 'aos'
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
 import Home from './Pages/Home';
+import Portfolio from './Components/Portfolio';
+import Footer from './Components/footer';
 function App() {
   useEffect(()=>{
     Aos.init({duration:2000})
@@ -13,13 +15,14 @@ function App() {
       <section>
           <div className="navbar-container">
 
-              <div className="navbar">
+              <div className="navbar" data-aos="fade-up">
                 <div className="logo-div">
                 <i class="fas fa-stream menu-toggler"></i>
                   <i class="fas fa-tablets logo"></i>
-                  <h1 className="brand"><strong className="abdul">Abdul</strong> <strong className="aziz">aziz</strong></h1>
+                  <h1 className="brand"><strong className="abdul">abdul</strong> <strong className="aziz">aziz</strong></h1>
+                  <i class="fas fa-tablets logo1"></i>
                 </div>
-
+                
                 <div className="nav-menu">
                   <ul>
                     <li>Home</li>
@@ -34,6 +37,18 @@ function App() {
 
       <section>
           <Home />
+      </section>
+
+      <section>
+      <div className="portfolio">
+        <div className="container">
+         <Portfolio />
+        </div>
+      </div>
+      </section>
+
+      <section>
+          <Footer />
       </section>
     </div>
   );
