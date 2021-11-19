f8b21859127d5307dfaff1c413dda5506232101c
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-11-19 16:39:28 +0300

    modif-7

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Footer from './Components/footer';
 import { BrowserRouter as Router , Link , Route ,Routes} from 'react-router-dom';
 import Contact from './Pages/Contact';
 import About from './Pages/About';
-import { useState } from 'react';
 
 function App() {
   useEffect(()=>{
