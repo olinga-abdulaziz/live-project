b4038ed243a835ad41201cab36e89f04bab397da
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-11-20 13:26:48 +0300

    modif-11

---
+++ src/Pages/Blogs.jsx
@@ -0,0 +1,8 @@
+function Blogs() {
+    return ( <div style={{height:"50vh"}}>
+        <center><strong>comming soon !!</strong></center>
+        <center><small>this page is under development ..</small></center>
+    </div> );
+}
+
+export default Blogs;
